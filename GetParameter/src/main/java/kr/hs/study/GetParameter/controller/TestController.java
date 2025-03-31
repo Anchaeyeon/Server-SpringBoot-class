a1c9c6c3b7b1507040c8a6b01f09b479f33eff07
Chaeyeon Ahn <s2328@e-mirim.hs.kr>
2025-03-31 13:28:00 +0900

    WebRequest, PathVariable, RequestParam, ModelAttribute 방식으로 값 추출하기

--- GetParameter/src/main/java/kr/hs/study/GetParameter/controller/TestController.java
+++ GetParameter/src/main/java/kr/hs/study/GetParameter/controller/TestController.java
@@ -1,8 +1,12 @@
 package kr.hs.study.GetParameter.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import kr.hs.study.GetParameter.dto.GetParameterDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
 
 @Controller
@@ -20,11 +24,42 @@ public class TestController {
         return "result";
     }
 
-    public String test1(WebRequest req) {
+    @GetMapping("/test2")
+    public String test2(WebRequest req) {
         String str1 = req.getParameter("data1");
         String str2 = req.getParameter("data2");
         System.out.println("data1 : "+str1);
         System.out.println("data2 : "+str2);
+        return "result";
+    }
+
+    @GetMapping("/test3/{data1}/{data2}/{data3}")
+    public String test3(@PathVariable("data1") int data1,
+                        @PathVariable("data2") int data2,
+                        @PathVariable("data3") int data3) {
+        System.out.println("data1:" + data1);
+        System.out.println("data2:" + data2);
+        System.out.println("data3:" + data3);
+        return "result";
+    }
+
+    @GetMapping("/test4")
+    public String test4(@RequestParam("data1") int data1,
+                        @RequestParam("data2") int data2,
+                        @RequestParam("data3") int[] data3) {
+        System.out.println("data1:" + data1);
+        System.out.println("data2:" + data2);
+        for (int a:data3) {
+            System.out.println("data3:" + a);
+        }
+        return "result";
+    }
+
+    @GetMapping("/test5")
+    //dto에 100,200이 들어가 있는 상태
+    public String test5(@ModelAttribute GetParameterDTO dto) {
+        System.out.println("data1 : " + dto.getData1());
+        System.out.println("data2 : " + dto.getData2());
         return "result";
     }
 }
