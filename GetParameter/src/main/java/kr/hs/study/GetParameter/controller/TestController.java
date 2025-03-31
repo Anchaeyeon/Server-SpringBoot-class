4cda797400660c6b8608323b89af5abc627b4846
Chaeyeon Ahn <s2328@e-mirim.hs.kr>
2025-03-31 11:26:51 +0900

    WebRequest 방법으로 값 넘기기

--- GetParameter/src/main/java/kr/hs/study/GetParameter/controller/TestController.java
+++ GetParameter/src/main/java/kr/hs/study/GetParameter/controller/TestController.java
@@ -3,11 +3,20 @@ package kr.hs.study.GetParameter.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class TestController {
     @GetMapping("/test1")
     public String test1(HttpServletRequest req) {
+        String str1 = req.getParameter("data1");
+        String str2 = req.getParameter("data2");
+        System.out.println("data1 : "+str1);
+        System.out.println("data2 : "+str2);
+        return "result";
+    }
+
+    public String test1(WebRequest req) {
         String str1 = req.getParameter("data1");
         String str2 = req.getParameter("data2");
         System.out.println("data1 : "+str1);
