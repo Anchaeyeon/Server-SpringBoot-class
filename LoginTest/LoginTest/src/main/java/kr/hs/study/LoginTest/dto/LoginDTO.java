2bd8fa9266796df4c3dc1ca08a4c6e856f15c557
Chaeyeon Ahn <s2328@e-mirim.hs.kr>
2025-04-07 13:43:43 +0900

    getter, setter 없어도 Data만 설정해주면 자동으로 만들어 줌

--- LoginTest/LoginTest/src/main/java/kr/hs/study/LoginTest/dto/LoginDTO.java
+++ LoginTest/LoginTest/src/main/java/kr/hs/study/LoginTest/dto/LoginDTO.java
@@ -1,22 +1,9 @@
 package kr.hs.study.LoginTest.dto;
 
+import lombok.Data;
+
+@Data  // set,get,constructor 다 들어있음
 public class LoginDTO {
     private String email;
     private Integer pw;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getPw() {
-        return pw;
-    }
-
-    public void setPw(int pw) {
-        this.pw = pw;
-    }
 }
