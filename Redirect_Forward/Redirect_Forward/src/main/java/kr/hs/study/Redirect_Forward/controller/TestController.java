5fec325369e36b2591d3f3f16f321237afa01f56
Chaeyeon Ahn <s2328@e-mirim.hs.kr>
2025-05-09 12:06:36 +0900

    forward / redirect 실습

---
+++ Redirect_Forward/Redirect_Forward/src/main/java/kr/hs/study/Redirect_Forward/controller/TestController.java
@@ -0,0 +1,26 @@
+package kr.hs.study.Redirect_Forward.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+    //forward
+    @GetMapping("/forward")
+    public String forward() {
+        return "forward:/sub1";
+        //sub1의 주소로 이동해라
+    }
+
+    @GetMapping("/sub1")
+    public String sub1() {
+        return "sub1";
+        //sub1: html 파일
+    }
+
+    //redirect
+    @GetMapping("/redirect")
+    public String redirect() {
+        return "redirect:/sub1";
+    }
+}
